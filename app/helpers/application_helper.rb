c38848b24e1fb05df22b989bb4f21bb07fc57a98
Tsuyoshi Nakayama <tsuyoshi@MacBook-Pro.local>
2020-09-27 07:48:17 +0900

    front side

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def user_icon(user)
+    if user.icon?
+      image_tag user.icon_url, class: 'icon'
+    else
+      image_tag('default.jpeg', class: 'icon')
+    end
+  end
+
+  def navigation_bar
+    if student_signed_in?
+      render 'students/nav'
+    elsif teacher_signed_in?
+      render 'teachers/nav'
+    end
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :room
 
+  validates :content, length: { maximum: 200 }
+
   mount_uploader :image, ImageUploader
 end
